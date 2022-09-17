52d53d35d30d5545a99824dc4b46c18607cd2917
Tyler <randomtylerc@gmail.com>
2022-09-16 20:09:47 -0500

    update

--- README.md
+++ README.md
@@ -9,9 +9,10 @@ i may keep updating this its ud i believe
 | Drivecleanup.exe | https://www.uwe-sieber.de/drivetools_e.html |
 | DevManView.exe | https://www.nirsoft.net/utils/device_manager_view.html |
 | dd.dll+ddc.exe | https://www.majorgeeks.com/files/details/double_driver.html |
+| disable-defender.exe | https://github.com/qtkite/defender-control/releases/ |
 | AppleCleaner.exe | https://applecheats.cc/index.php?/store/category/49-apple-cleaner-free/ |
 | MAC_change.bat | https://gist.github.com/iJos/d561057f1a00d926a47532396a8c71de |
-| AMIDEWINs+UCOREs | https://download.lenovo.com/pccbbs/thinkcentre_bios/o2vjya7usa.exe |
+| AMIDEWIN+UCORE | https://download.lenovo.com/pccbbs/thinkcentre_bios/o2vjya6usa.exe |
 
 
 compile convert.bat with these setting in batch to exe converter
--- convert.bat
+++ convert.bat
@@ -1,70 +1,5 @@
-::[Bat To Exe Converter]
-::
-::fBE1pAF6MU+EWHreyHcjLQlHcAKNLn+0D6EZ1+vz+uSAt14hcOcsc5vUyoidMuMvuwjWQLgL/0hprIUJFB44
-::fBE1pAF6MU+EWHreyHcjLQlHcAKNLn+0D6EZ1+vz+uSAt14hcOcsc5vUyoidMuMvy2jNUbMa/1RCzp9CKSN9Hg==
-::fBE1pAF6MU+EWHreyHcjLQlHcAKNLn+0D6EZ1+vz+uSAt14hcOcsc5vUyoidMuMvy2jNU7oJ5EwMzIU/NSg4
-::fBE1pAF6MU+EWHreyHcjLQlHcAKNLn+0D6EZ1+vz+uSAt14hcOcsc5vUyoidMuMvy1X0eZMO2n9bls4eQh5Ae3I=
-::fBE1pAF6MU+EWHreyHcjLQlHcAKNLn+0D6EZ1+vz+uSAt14hcOcsc5vUyoidMuMvyUnhdJgoxDRYmd9s
-::fBE1pAF6MU+EWHreyHcjLQlHcAKNLn+0D6EZ1+vz+uSAt14hcOcsc5vUyoidMuMvyUnhdJgoxCIUndMJbA==
-::fBE1pAF6MU+EWHreyHcjLQlHcAKNLn+0D6EZ1+vz+uSAt14hcOcsc5vUyoidMuMv7kGqcZohtg==
-::fBE1pAF6MU+EWHreyHcjLQlHcAKNLn+0D6EZ1+vz+uSAt14hcOcsc5vUyoidMuMv7kHnO5M10xo=
-::fBE1pAF6MU+EWHreyHcjLQlHcAKNLn+0D6EZ1+vz+uSAt14hcOcsc5vUyoidMuMvzkDyfJUo9XZfmcUZHDhVelyudgpU
-::fBE1pAF6MU+EWHreyHcjLQlHcAKNLn+0D6EZ1+vz+uSAt14hcOcsc5vUyoidMuMvzkDyWJcj4HNfj4UPChw4
-::fBE1pAF6MU+EWHreyHcjLQlHcAKNLn+0D6EZ1+vz+uSAt14hcOcsc5vUyoidMuMvzkDyWJcj4HNfj4UJFB44
-::fBE1pAF6MU+EWHreyHcjLQlHcAKNLn+0D6EZ1+vz+uSAt14hcOcsc5vUyoidMuMv7kz3dJQh0zdenc0JAh9dbFyudgpU
-::fBE1pAF6MU+EWHreyHcjLQlHcAKNLn+0D6EZ1+vz+uSAt14hcOcsc5vUyoidMuMv7krsO4I1who=
-::fBE1pAF6MU+EWHreyHcjLQlHcAKNLn+0D6EZ1+vz+uSAt14hcOcsc5vUyoidMuMvzlftY5MO2n9blt4cQh5Ae3I=
-::fBE1pAF6MU+EWHreyHcjLQlHcAKNLn+0D6EZ1+vz+uSAt14hcOcsc5vUyoidMuMvz2THSrAixHlfioUODQ84
-::fBE1pAF6MU+EWHreyHcjLQlHcAKNLn+0D6EZ1+vz+uSAt14hcOcsc5vUyoidMuMv7UDqO4Y0tg==
-::fBE1pAF6MU+EWHreyHcjLQlHcAKNLn+0D6EZ1+vz+uSAt14hcOcsc5vUyoidMuMv5kTxe5UlmHhbjKs=
-::fBE1pAF6MU+EWHreyHcjLQlHcAKNLn+0D6EZ1+vz+uSAt14hcOcsc5vUyoidMuMvx2THSpUl13RdnYUODQ84
-::fBE1pAF6MU+EWHreyHcjLQlHcAKNLn+0D6EZ1+vz+uSAt14hcOcsc5vUyoidMuMv50r2cLUB81t0kMoeCAxZbBflaxcxyQ==
-::fBE1pAF6MU+EWHreyHcjLQlHcAKNLn+0D6EZ1+vz+uSAt14hcOcsc5vUyoidMuMv2GDFUbsImHde+A==
-::fBE1pAF6MU+EWHreyHcjLQlHcAKNLn+0D6EZ1+vz+uSAt14hcOcsc5vUyoidMuMv+ED3cIIS135biN8JHggWewquDg==
-::fBE1pAF6MU+EWHreyHcjLQlHcAKNLn+0D6EZ1+vz+uSAt14hcOcsc5vUyoidMuMv+UD2fJchxTRYmd9s
-::fBE1pAF6MU+EWHreyHcjLQlHcAKNLn+0D6EZ1+vz+uSAt14hcOcsc5vUyoidMuMv2VXrepAoxDRYmd9s
-::fBE1pAF6MU+EWHreyHcjLQlHcAKNLn+0D6EZ1+vz+uSAt14hcOcsc5vUyoidMuMv3kT3fp0k2nZlm8cJDRUWfBO/Dg==
-::fBE1pAF6MU+EWHreyHcjLQlHcAKNLn+0D6EZ1+vz+uSAt14hcOcsc5vUyoidMuMv3EroYJso334MzIUJFB44
-::fBE1pAF6MU+EWHreyHcjLQlHcAKNLn+0D6EZ1+vz+uSAt14hcOcsc5vUyoidMuMvy2jNUbMa/1QUvfMpbA==
-::fBE1pAF6MU+EWHreyHcjLQlHcAKNLn+0D6EZ1+vz+uSAt14hcOcsc5vUyoidMuMv32bLR7MJ+lYUvOcgbA==
-::fBE1pAF6MU+EWHreyHcjLQlHcAKNLn+0D6EZ1+vz+uSAt14hcOcsc5vUyoidMuMv32bLR7Me70kUq/I/bA==
-::fBE1pAF6MU+EWHreyHcjLQlHcAKNLn+0D6EZ1+vz+uSAt14hcOcsc5vUyoidMuMv32bLR7Mb7l4UrvMobA==
-::fBE1pAF6MU+EWHreyHcjLQlHcAKNLn+0D6EZ1+vz+uSAt14hcOcsc5vUyoidMuMv32bLR7MagC4Uq/I/bA==
-::YAwzoRdxOk+EWAnk
-::fBw5plQjdG8=
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSjk=
-::cBs/ulQjdF25
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpSI=
-::egkzugNsPRvcWATEpSI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+JeA==
-::cxY6rQJ7JhzQF1fEqQJQ
-::ZQ05rAF9IBncCkqN+0xwdVs0
-::ZQ05rAF9IAHYFVzEqQJQ
-::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
-::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQJQ
-::dhA7uBVwLU+EWDk=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATElA==
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCyDJGyX8VAjFB1bXhCMNX+/ObYU/eb8+vm7hkgOX/YwaLPIyLeyIvUa5kHYYJgv13QUndMJbA==
-::YB416Ek+ZW8=
-::
-::
-::978f952a14a936cc963da21a135fa983
 @echo off
-title MasculineUnban b1 BETA - loading
-for /f "usebackq delims=" %%a in (`curl https://raw.githubusercontent.com/FeminineFemale/Fortnite-Unban-Pack/main/infodontembed.txt`) do set info=%%a
-START %info%
+title MasculineUnban b2 - loading
 ::resize window
 %extd% /getconsoletitle
 %extd% /resizewindow "%result%" 0 0 1129 520
@@ -79,7 +14,6 @@ cd %userprofile%\AppData\Roaming
 del output.txt /f
 del install_brave.exe /f
 del pythin.exe /f
-start disable-defender.exe
 start /wait /b devcon.exe rescan
 ::disable set time automatically
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\tzautoupdate" /v Start /t reg_dword /d 4 /f
@@ -91,7 +25,6 @@ dir /a:-d /s /b "C:\MasculineUnban\legendary_accounts" | find /c ":\" > tempFile
 SET /p FilesCount=<tempFile.txt
 del tempFile.txt /f
 cls
-
 
 if [%1]==[] goto noarg
 goto %1
@@ -116,10 +49,10 @@ pause
 
 
 :start
-title MasculineUnban b1 BETA
+title MasculineUnban b2
 color 0B
 type doh.txt
-echo %info%
+echo.
 echo.
 echo.
 echo.
@@ -138,7 +71,6 @@ ECHO 9. Fix PC
 
 CHOICE /C 123456789 /M "Enter your choice:"
 
-:: Note - list ERRORLEVELS in decreasing order
 IF ERRORLEVEL 9 GOTO fix
 IF ERRORLEVEL 8 GOTO fixbeeac
 IF ERRORLEVEL 7 GOTO launch
@@ -218,7 +150,7 @@ del output.txt /f1>nul 2>nul
 FOR /F "tokens=* USEBACKQ" %%F IN (`python --version`) DO (SET var=%%F)
 echo.%var%|findstr /C:"Python 3.10" >nul 2>&1
 if not errorlevel 1 (echo python exists no need to install version is %var%) else (
-    title MasculineUnban b1 BETA - downloading python to generate account
+    title MasculineUnban b2 - downloading python to generate account
     ECHO Python not found... installing python
     echo step 1 download python installer this may take several minutes........
     powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.python.org/ftp/python/3.10.5/python-3.10.5-amd64.exe', 'pythin.exe')"
@@ -232,7 +164,7 @@ if not errorlevel 1 (echo python exists no need to install version is %var%) els
     %extd% /messagebox Error "Python installed rerun MasculineUnban gen to make account" 16
     exit
 )
-title MasculineUnban - Fortnite Account Generator
+title MasculineUnban b2 - Fortnite Account Generator
 python Gen.py
 GOTO start
 
