a71d08fa3b3a2bbe44e92fa7faf6970ea12045f9
Tyler <randomtylerc@gmail.com>
2022-10-07 22:07:38 -0500

    remove disable defender

--- gen.py
+++ gen.py
@@ -136,6 +136,7 @@ if __name__ == '__main__':
     import legendary
     import undetected_chromedriver as uc
     from selenium.webdriver.common.by import By
+    from selenium.webdriver.support.select import Select
     from selenium.webdriver.common.action_chains import ActionChains
     from selenium.webdriver.support import expected_conditions as EC
     from selenium.webdriver.support.ui import WebDriverWait
