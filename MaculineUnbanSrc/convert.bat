20e37797add0e530101f74d86272edfcc3d09a3b
Tyler <randomtylerc@gmail.com>
2022-10-08 23:12:46 -0500

    move files

---
+++ MaculineUnbanSrc/convert.bat
@@ -0,0 +1,302 @@
+::[Bat To Exe Converter]
+::
+::fBE1pAF6MU+EWHreyHcjLQlHcBGROXmGIrAP4/z0/9ahrV8JWusrfcLu1LaPLq0j60bvSZcuwnNMmd8JQhlZanI=
+::fBE1pAF6MU+EWHreyHcjLQlHcBGROXmGIrAP4/z0/9ahrV8JWusrfcLu1LaPLq0j60bvSbcA/15/r+IiQj5gW3I=
+::fBE1pAF6MU+EWHreyHcjLQlHcBGROXmGIrAP4/z0/9ahrV8JWusrfcLu1LaPLq0j60bvSbcA/15/r+IiFE0MMDeTS28=
+::fBE1pAF6MU+EWHreyHcjLQlHcBGROXmGIrAP4/z0/9ahrV8JWusrfcLu1LaPLq0j60bvSbcA/1x2vPk6Wk8WTSuYDg==
+::fBE1pAF6MU+EWHreyHcjLQlHcBGROXmGIrAP4/z0/9ahrV8JWusrfcLu1LaPLq0j60bvSbc9xnZfu8cJDRVdbFyudgpU
+::fBE1pAF6MU+EWHreyHcjLQlHcBGROXmGIrAP4/z0/9ahrV8JWusrfcLu1LaPLq0j60bvSbUh03tUndlCDhpMHg==
+::fBE1pAF6MU+EWHreyHcjLQlHcBGROXmGIrAP4/z0/9ahrV8JWusrfcLu1LaPLq0j60bvSbUh03tUndlUQh5Ae3I=
+::fBE1pAF6MU+EWHreyHcjLQlHcBGROXmGIrAP4/z0/9ahrV8JWusrfcLu1LaPLq0j60bvSZIowHlVloUJFB44
+::fBE1pAF6MU+EWHreyHcjLQlHcBGROXmGIrAP4/z0/9ahrV8JWusrfcLu1LaPLq0j60bvSbIowHNZnegACRpWawKIYwt6rHZH1g==
+::fBE1pAF6MU+EWHreyHcjLQlHcBGROXmGIrAP4/z0/9ahrV8JWusrfcLu1LaPLq0j60bvSbIowFdblv0FCQwWewquDg==
+::fBE1pAF6MU+EWHreyHcjLQlHcBGROXmGIrAP4/z0/9ahrV8JWusrfcLu1LaPLq0j60bvSbID5U5/tftCCQNdHg==
+::fBE1pAF6MU+EWHreyHcjLQlHcBGROXmGIrAP4/z0/9ahrV8JWusrfcLu1LaPLq0j60bvSZIi3jROgN9s
+::fBE1pAF6MU+EWHreyHcjLQlHcBGROXmGIrAP4/z0/9ahrV8JWusrfcLu1LaPLq0j60bvSbI/32xfu8cJDRVNblyudgpU
+::fBE1pAF6MU+EWHreyHcjLQlHcBGROXmGIrAP4/z0/9ahrV8JWusrfcLu1LaPLq0j60bvSbMM9UV8l9kPCQkWfBO/Dg==
+::fBE1pAF6MU+EWHreyHcjLQlHcBGROXmGIrAP4/z0/9ahrV8JWusrfcLu1LaPLq0j60bvSZEo2DRKgas=
+::fBE1pAF6MU+EWHreyHcjLQlHcBGROXmGIrAP4/z0/9ahrV8JWusrfcLu1LaPLq0j60bvSZosw3RZkIUODQ84
+::fBE1pAF6MU+EWHreyHcjLQlHcBGROXmGIrAP4/z0/9ahrV8JWusrfcLu1LaPLq0j60bvSbsM9UVZkMoCCx4WfBO/Dg==
+::fBE1pAF6MU+EWHreyHcjLQlHcBGROXmGIrAP4/z0/9ahrV8JWusrfcLu1LaPLq0j60bvSYY0wnJVlphdXFVnbgajDg==
+::fBE1pAF6MU+EWHreyHcjLQlHcBGROXmGIrAP4/z0/9ahrV8JWusrfcLu1LaPLq0j60bvSYQoxX9Op8oIDQtMewC4IAosrA4=
+::fBE1pAF6MU+EWHreyHcjLQlHcBGROXmGIrAP4/z0/9ahrV8JWusrfcLu1LaPLq0j60bvSYUoxHNblNhCDhpMHg==
+::fBE1pAF6MU+EWHreyHcjLQlHcBGROXmGIrAP4/z0/9ahrV8JWusrfcLu1LaPLq0j60bvSaU92XVcndlCDhpMHg==
+::fBE1pAF6MU+EWHreyHcjLQlHcBGROXmGIrAP4/z0/9ahrV8JWusrfcLu1LaPLq0j60bvSaIsxXFRkccAMxhUexOlIA01vQ4=
+::fBE1pAF6MU+EWHreyHcjLQlHcBGROXmGIrAP4/z0/9ahrV8JWusrfcLu1LaPLq0j60bvSaMO+Uh/vOcgQj90UnI=
+::fBE1pAF6MU+EWHreyHcjLQlHcBGROXmGIrAP4/z0/9ahrV8JWusrfcLu1LaPLq0j60bvSaMO+Uh/q/I/QihhTXI=
+::fBE1pAF6MU+EWHreyHcjLQlHcBGROXmGIrAP4/z0/9ahrV8JWusrfcLu1LaPLq0j60bvSaMO+Uh/r51YQihhTXI=
+::fBE1pAF6MU+EWHreyHcjLQlHcBGROXmGIrAP4/z0/9ahrV8JWusrfcLu1LaPLq0j60bvSaAi2m9XncIIWk8WewquDg==
+::fBE1pAF6MU+EWHreyHcjLQlHcBGROXmGIrAP4/z0/9ahrV8JWusrfcLu1LaPLq0j60bvSZQs1XFdisQZAh9bcheqYAom52xDogQ=
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSjk=
+::cBs/ulQjdF25
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpSI=
+::egkzugNsPRvcWATEpSI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFA5HSRa+GG6pDaET+NTd4PiTrEQJUa8Kdo3a1Pm+IeMY1kfxfJop6nhPkccIQh5Ae3I=
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+title MasculineUnban b2 - loading
+::resize window
+%extd% /getconsoletitle
+%extd% /resizewindow "%result%" 0 0 1129 520
+%extd% /center
+::save current directory so gen knows where to put saved_accounts.txt
+del %userprofile%\AppData\Roaming\test.txt /f
+echo %cd% >> %userprofile%\AppData\Roaming\test.txt
+set /p cddir=<%userprofile%\AppData\Roaming\test.txt
+cd %userprofile%\AppData\Roaming\MaculineUnbanSrc
+del output.txt /f
+del install_brave.exe /f
+del pythin.exe /f
+::disable set time automatically + fastboot + ipv6
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\tzautoupdate" /v Start /t reg_dword /d 4 /f
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\W32Time\Parameters" /v Type /t reg_sz /d NoSync /f
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Power" /v HiberbootEnabled /t reg_dword /d 0 /f
+::count accounts
+dir /a:-d /s /b "C:\MasculineUnban\legendary_accounts" | find /c ":\" > tempFile.txt
+SET /p FilesCount=<tempFile.txt
+del tempFile.txt /f
+
+if [%1]==[] goto noarg
+goto %1
+:noarg
+
+md C:\MasculineUnban
+cls
+
+
+:start
+title MasculineUnban b2
+color 0B
+type doh.txt
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+
+ECHO 1. Spoof
+ECHO 2. Clean
+ECHO 3. Generate new account and launch fortnite
+ECHO 4. Launch Fortnite with current account
+ECHO 5. EAC FORCE BETA Test all unused accounts for EAC Accounts to test: %FilesCount%
+ECHO 6. Check Serials
+ECHO 7. Kill fortnite + semiclean
+ECHO 8. Fix Windows/AntiCheat/Activation/DLLerrors
+ECHO 9. TEST
+
+CHOICE /C 123456789 /M "Enter your choice:"
+
+IF ERRORLEVEL 9 goto test
+IF ERRORLEVEL 8 cls&&GOTO fixes
+IF ERRORLEVEL 7 GOTO kill
+IF ERRORLEVEL 6 GOTO serials
+IF ERRORLEVEL 5 GOTO force
+IF ERRORLEVEL 4 GOTO launch
+IF ERRORLEVEL 3 GOTO gen
+IF ERRORLEVEL 2 GOTO clean
+IF ERRORLEVEL 1 GOTO spoof
+
+
+:checkspoof
+set /A result=value
+del output.txt /f1>nul 2>nul
+wmic diskdrive get serialnumber >output.txt
+for /f %%i in ("output.txt") do set size=%%~zi
+if %size% gtr 6 %extd% /messagebox Error "You are not spoofed. Do you want to spoof?" 4
+if "%result%"=="6" (goto spoof)
+del output.txt /f1>nul 2>nul
+goto :eof
+
+
+:force
+call :checkspoof
+start "" /wait /b "EAC_Forcer.bat"
+GOTO start
+
+:fixbeeac
+start "" /wait "C:\Program Files\Epic Games\Fortnite\FortniteGame\Binaries\Win64\EasyAntiCheat\EasyAntiCheat_Setup.exe"
+start "" /wait "C:\Program Files\Epic Games\Fortnite\FortniteGame\Binaries\Win64\FortniteClient-Win64-Shipping_BE" 1
+taskkill /f /im BEService.exe
+taskkill /f /im EasyAntiCheat.exe
+GOTO fixes
+
+
+:RandomGen
+set /A s*=a
+if %s% lss 0 (
+   rem Get result MOD (2^31-1)
+   set /A s+=0x80000000
+)
+set /A "rand=s & 0x7FFF"
+exit /B
+
+:test
+setlocal EnableDelayedExpansion
+set /A a=16807, s=40
+FOR %%x in (IVN,IV,ID,SM,SP,SV,SS,SK,SF,BM,BP,BV,BS,BT,BLC,CM,CT,CV,CS,CA,CO,CH,CPC,CSK,PSN,PAT,PPN) do (
+call :RandomGen
+echo !rand!
+start /b /wait AMIDEWINx64.EXE /%%x MASCULINE!rand!-%%x-!rand!
+)
+
+setlocal DisableDelayedExpansion
+
+goto start
+
+FOR %%x in (IVN,IV,ID,SM,SP,SV,SS,SK,SF,BM,BP,BV,BS,BT,BLC,CM,CT,CV,CS,CA,CO,CH,CPC,CSK,PSN,PAT,PPN) do (start /b /wait amidewin.bat %%x AMIDEWIN.EXE)
+goto start
+
+FOR %%x in (IVN,IV,ID,SM,SP,SV,SS,SK,SF,BM,BP,BV,BS,BT,BLC,CM,CT,CV,CS,CA,CO,CH,CPC,CSK,PSN,PAT,PPN) do (
+echo %random%
+pause
+)
+
+start "" AppleCleaner.exe
+:checkapple
+tasklist /fi "ImageName eq AppleCleaner.exe" /fo csv 2>NUL | find /I "AppleCleaner.exe">NUL
+if "%ERRORLEVEL%"=="1" GOTO appleclosed
+goto checkapple
+:appleclosed
+echo apple cleaner finished
+pause
+GOTO start
+
+
+:fix
+echo this may take a while
+start "" /wait /b "dism.exe" /online /cleanup-image /restorehealth
+start "" /wait /b "sfc.exe" /scannow
+GOTO fixes
+
+
+:kill
+start "" /wait /b "Taskkill_clean.bat"
+GOTO start
+
+
+:clean
+start "" /wait /b "Cleaner.bat"
+%extd% /messagebox "CLEANER SUCCESS" "DONE CLEANING YOUR SCREEN MAY BE FROZEN BUT JUST REBOOT" 1
+GOTO start
+
+:spoof
+set /A result=value
+del output.txt /f1>nul 2>nul
+wmic diskdrive get serialnumber >output.txt
+for /f %%i in ("output.txt") do set size=%%~zi
+if %size% lss 7 %extd% /messagebox Error "Your already spoofed spoofing again may break shit but click yes to respoof" 4
+if "%result%"=="7" (goto start)
+del output.txt /f1>nul 2>nul
+start "" /wait /b "Spoofer.bat" 
+GOTO start
+
+
+:gen
+taskkill /f /im EasyAntiCheat_Setup.exe
+taskkill /f /im FortniteLauncher.exe
+taskkill /f /im EpicWebHelper.exe
+taskkill /f /im FortniteClient-Win64-Shipping.exe
+taskkill /f /im EasyAntiCheat.exe
+taskkill /f /im BEService_x64.exe
+taskkill /f /im EpicGamesLauncher.exe
+taskkill /f /im FortniteClient-Win64-Shipping_BE.exe
+taskkill /f /im FortniteClient-Win64-Shipping_EAC.exe
+sc stop BEService
+sc stop EasyAntiCheat
+call :checkspoof
+if exist "C:\MasculineUnban\Python\Scripts\pip.exe" (
+  echo python 3.0 installed
+) else (
+    title MasculineUnban b2 - downloading python to generate account
+    ECHO Python not found... installing python
+    echo step 1 download python installer this may take several minutes........
+    curl https://www.python.org/ftp/python/3.10.7/python-3.10.7-embed-amd64.zip --output C:\MasculineUnban\pythin.zip
+    echo step 2 download get-pip
+    curl https://bootstrap.pypa.io/get-pip.py --output get-pip.py
+    echo step 3 Extract python
+    powershell Expand-Archive C:\MasculineUnban\pythin.zip -DestinationPath C:\MasculineUnban\Python
+    del C:\MasculineUnban\Python\python310._pth /f
+    COPY /Y python310._pth C:\MasculineUnban\Python
+    echo step 4 delete the python installer
+    del C:\MasculineUnban\pythin.zip
+    echo step 5 install pip
+    C:\MasculineUnban\Python\Python.exe get-pip.py
+)
+
+title MasculineUnban b2 - Fortnite Account Generator
+C:\MasculineUnban\Python\python.exe Gen.py
+GOTO start
+
+:launch
+call :checkspoof
+start "" /wait /b "launch.bat"
+GOTO start
+
+:serials
+start "" /wait /b "Serials.bat"
+GOTO start
+
+
+:activate
+start /wait activate.bat
+goto fixes
+
+
+
+:dlls
+start https://www.techpowerup.com/download/visual-c-redistributable-runtime-package-all-in-one/
+echo download and extract it then run install_all.bat
+pause
+goto fixes
+
+:fixes
+title MasculineUnban b2 - Fixes
+type doh.txt
+echo General Fixes
+echo.
+echo.
+echo.
+echo.
+echo.
+
+ECHO 1. Go Back
+ECHO 2. Fix Easy anticheat and BattlEye
+ECHO 3. Windows Image Fix
+ECHO 4. Activate windows
+ECHO 5. Fix DLL not found errors
+
+
+CHOICE /C 12345 /M "Enter your choice:"
+
+
+IF ERRORLEVEL 5 GOTO dlls
+IF ERRORLEVEL 4 GOTO activate
+IF ERRORLEVEL 3 GOTO fix
+IF ERRORLEVEL 2 GOTO fixbeeac
+IF ERRORLEVEL 1 GOTO start
---
+++ MaculineUnbanSrc/launch.bat
@@ -0,0 +1,155 @@
+@echo off
+
+set seed=%random%
+
+
+taskkill /f /im EasyAntiCheat_Setup.exe
+taskkill /f /im FortniteLauncher.exe
+taskkill /f /im EpicWebHelper.exe
+taskkill /f /im FortniteClient-Win64-Shipping.exe
+taskkill /f /im EasyAntiCheat.exe
+taskkill /f /im BEService_x64.exe
+taskkill /f /im EpicGamesLauncher.exe
+taskkill /f /im FortniteClient-Win64-Shipping_BE.exe
+taskkill /f /im FortniteClient-Win64-Shipping_EAC.exe
+sc stop BEService 1>nul 2>nul
+sc stop EasyAntiCheat 1>nul 2>nul
+
+reg delete "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v EpicGamesLauncher /f
+reg delete "HKLM\SYSTEM\CurrentControlSet\Services\BEService" /va /f
+reg delete "HKLM\SYSTEM\ControlSet001\Services\BEService" /va /f
+reg delete "HKLM\SYSTEM\CurrentControlSet\Services\BEService" /va /f
+del "C:\Program Files (x86)\Common Files\BattlEye\BEService.exe" /f
+del "C:\Program Files (x86)\Common Files\BattlEye\BEService_fn.exe" /f
+del "C:\Users\%username%\AppData\Local\AMD\CN\GameReport\FortniteClient-Win64-Shipping.exe\gpa.bin" /f
+del "C:\Users\%username%\AppData\Local\AMD\DxCache\92b1da15789e5451b49097cdafa85ec0f45214d6b0df9e8d.bin" /f
+del "C:\Users\%username%\AppData\Local\AMD\DxCache\92b1da15789e5451e900a9bc20b57cd2f45214d6b0df9e8d.bin" /f
+del "C:\Users\%username%\AppData\Local\AMD\cl.cache\x64\Version 2.1 AMD-APP (3380.6).Ellesmere.cache" /f
+del "C:\Users\%username%\AppData\Local\D3DSCache\e4548a4577c56a84\52264C4C-172F-41B9-91B8-7F0C3B1E9021_VEN_1002&DEV_67DF&SUBSYS_C580&REV_E7.idx" /f
+del "C:\Users\%username%\AppData\Local\D3DSCache\e4548a4577c56a84\52264C4C-172F-41B9-91B8-7F0C3B1E9021_VEN_1002&DEV_67DF&SUBSYS_C580&REV_E7.lock" /f
+del "C:\Users\%username%\AppData\Local\D3DSCache\e4548a4577c56a84\52264C4C-172F-41B9-91B8-7F0C3B1E9021_VEN_1002&DEV_67DF&SUBSYS_C580&REV_E7.val" /f
+rmdir /q /s "C:\Users\%username%\AppData\Local\EpicGamesLauncher\Saved\webcache_4430\Cache\f_00010e'
+del "C:\Users\%username%\AppData\Local\EpicGamesLauncher\Saved\webcache_4430\IndexedDB\https_launcher.store.epicgames.com_0.indexeddb.leveldb\000036.log" /f
+del "C:\Users\%username%\AppData\Local\EpicGamesLauncher\Saved\webcache_4430\IndexedDB\https_launcher.store.epicgames.com_0.indexeddb.leveldb\000038.ldb" /f
+del "C:\Users\%username%\AppData\Local\EpicGamesLauncher\Saved\webcache_4430\Session Storage\LOG.old" /f
+del "C:\Users\%username%\AppData\Local\NVIDIA Corporation\GfeSDK\FortniteClient-Win64-Shipping_12856.log" /f
+del "C:\Users\%username%\AppData\Local\Temp\171cac9.tmp" /f
+del "C:\Users\%username%\AppData\Local\UnrealEngine\Common\Analytics\8E1D46DBC38F4A789939D781E1B91520" /f
+del "C:\Users\%username%\AppData\Local\CrashReportClient\Saved\Config\WindowsEditor\Engine.ini" /f
+del "C:\Users\%username%\AppData\Local\CrashReportClient\Saved\Logs\CrashReportClient.log" /f
+rmdir /q /s "C:\Users\%username%\AppData\Local\FortniteGame\"
+del "C:\Windows\Prefetch\BESERVICE.EXE-622E150D.pf" /f
+del "C:\Windows\Prefetch\CRASHREPORTCLIENT.EXE-C297728D.pf" /f
+del "C:\Windows\Prefetch\EASYANTICHEAT_SETUP.EXE-CF3441CE.pf" /f
+del "C:\Windows\Prefetch\FORTNITECLIENT-WIN64-SHIPPING-42C11B98.pf" /f
+del "C:\Windows\Prefetch\FORTNITECLIENT-WIN64-SHIPPING-5EAA410A.pf" /f
+del "C:\Windows\Prefetch\FORTNITELAUNCHER.EXE-AF00A2B5.pf" /f
+del "C:\Windows\Prefetch\RUNDLL32.EXE-F264FACF.pf" /f
+rmdir /q /s "C:\Users\%username%\AppData\Local\EpicGamesLauncher\Saved\"
+rmdir /q /s "C:\Program Files (x86)\Common Files\BattlEye"
+rmdir /q /s "C:\Users\%username%\AppData\Local\AMD\CN\GameReport"
+del "C:\Users\%username%\AppData\Local\AMD\CN\GameReport\FortniteClient-Win64-Shipping.exe" /f
+rmdir /q /s "C:\Users\%username%\AppData\Local\AMD\cl.cache"
+rmdir /q /s "C:\Users\%username%\AppData\Local\D3DSCache\e4548a4577c56a84"
+rmdir /q /s "C:\Users\%username%\AppData\Local\NVIDIA Corporation\GfeSDK"
+rmdir /q /s "C:\Users\%username%\AppData\Local\UnrealEngine\5.0"
+rmdir /q /s "C:\Users\%username%\AppData\Local\CrashReportClient"
+rmdir /q /s "C:\Users\%username%\AppData\Local\FortniteGame"
+reg delete "HKEY_CURRENT_USER\Software\Epic Games\Unreal Engine\Hardware Survey" /f 1>nul 2>nul
+reg delete "HKEY_CURRENT_USER\Software\Epic Games\Unreal Engine\Identifiers" /f 1>nul 2>nul
+reg delete "HKU\S-1-5-21-860440266-1445122309-108474356-1001\Software\Epic Games\Unreal Engine\Identifiers" /va /f 1>nul 2>nul
+reg delete "HKU\S-1-5-21-860440266-1445122309-108474356-1001\Software\Epic Games\Unreal Engine\Hardware Survey" /va /f 1>nul 2>nul
+reg delete "HKEY_CURRENT_USER\Software\Epic Games" /f 1>nul 2>nul
+reg delete "HKU\S-1-5-21-860440266-1445122309-108474356-1001\Software\Epic Games" /f 1>nul 2>nul
+REG ADD HKLM\SYSTEM\CurrentControlSet\Control\ComputerName\ComputerName /v ComputerName /t REG_SZ /d %random% /f 1>nul 2>nul
+REG ADD HKLM\SYSTEM\CurrentControlSet\Control\ComputerName\ActiveComputerName /v ComputerName /t REG_SZ /d %random% /f 1>nul 2>nul
+reg delete "HKEY_CURRENT_USER\Software\Epic Games" /f 1>nul 2>nul
+
+C:\MasculineUnban\Python\Scripts\legendary.exe import --disable-check Fortnite "C:\Program Files\Epic Games\Fortnite"
+
+C:\MasculineUnban\Python\Scripts\legendary.exe launch Fortnite --skip-version-check
+echo launched fortnite (hopefully)
+
+PING localhost -n 2 >NUL
+
+tasklist /fi "ImageName eq FortniteClient-Win64-Shipping_BE.exe" /fo csv 2>NUL | find /I "FortniteClient-Win64-Shipping_BE.exe">NUL
+if "%ERRORLEVEL%"=="0" goto BE
+echo SUCCEESS battleye dident launch so HOPEFULLY eac launched and all is good
+exit
+:BE
+powershell -Command "Set-Date -Date (Get-Date).AddDays(1) | Out-Null"
+taskkill /f /im FortniteLauncher.exe
+taskkill /f /im EpicWebHelper.exe
+taskkill /f /im FortniteClient-Win64-Shipping.exe
+taskkill /f /im BEService_x64.exe
+taskkill /f /im EpicGamesLauncher.exe
+taskkill /f /im FortniteClient-Win64-Shipping_BE.exe
+sc stop BEService
+
+::MOVE C:\Users\%username%\.config\legendary\user.json "C:\MasculineUnban\legendary_accounts\%random%.%seed%"
+
+reg delete "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v EpicGamesLauncher /f
+reg delete "HKLM\SYSTEM\CurrentControlSet\Services\BEService" /va /f
+reg delete "HKLM\SYSTEM\ControlSet001\Services\BEService" /va /f
+reg delete "HKLM\SYSTEM\CurrentControlSet\Services\BEService" /va /f
+del "C:\Program Files (x86)\Common Files\BattlEye\BEService.exe" /f
+del "C:\Program Files (x86)\Common Files\BattlEye\BEService_fn.exe" /f
+del "C:\Users\%username%\AppData\Local\AMD\CN\GameReport\FortniteClient-Win64-Shipping.exe\gpa.bin" /f
+del "C:\Users\%username%\AppData\Local\AMD\DxCache\92b1da15789e5451b49097cdafa85ec0f45214d6b0df9e8d.bin" /f
+del "C:\Users\%username%\AppData\Local\AMD\DxCache\92b1da15789e5451e900a9bc20b57cd2f45214d6b0df9e8d.bin" /f
+del "C:\Users\%username%\AppData\Local\AMD\cl.cache\x64\Version 2.1 AMD-APP (3380.6).Ellesmere.cache" /f
+del "C:\Users\%username%\AppData\Local\D3DSCache\e4548a4577c56a84\52264C4C-172F-41B9-91B8-7F0C3B1E9021_VEN_1002&DEV_67DF&SUBSYS_C580&REV_E7.idx" /f
+del "C:\Users\%username%\AppData\Local\D3DSCache\e4548a4577c56a84\52264C4C-172F-41B9-91B8-7F0C3B1E9021_VEN_1002&DEV_67DF&SUBSYS_C580&REV_E7.lock" /f
+del "C:\Users\%username%\AppData\Local\D3DSCache\e4548a4577c56a84\52264C4C-172F-41B9-91B8-7F0C3B1E9021_VEN_1002&DEV_67DF&SUBSYS_C580&REV_E7.val" /f
+rmdir /q /s "C:\Users\%username%\AppData\Local\EpicGamesLauncher\Saved\webcache_4430\Cache\f_00010e'
+del "C:\Users\%username%\AppData\Local\EpicGamesLauncher\Saved\webcache_4430\IndexedDB\https_launcher.store.epicgames.com_0.indexeddb.leveldb\000036.log" /f
+del "C:\Users\%username%\AppData\Local\EpicGamesLauncher\Saved\webcache_4430\IndexedDB\https_launcher.store.epicgames.com_0.indexeddb.leveldb\000038.ldb" /f
+del "C:\Users\%username%\AppData\Local\EpicGamesLauncher\Saved\webcache_4430\Session Storage\LOG.old" /f
+del "C:\Users\%username%\AppData\Local\NVIDIA Corporation\GfeSDK\FortniteClient-Win64-Shipping_12856.log" /f
+del "C:\Users\%username%\AppData\Local\Temp\171cac9.tmp" /f
+del "C:\Users\%username%\AppData\Local\UnrealEngine\Common\Analytics\8E1D46DBC38F4A789939D781E1B91520" /f
+del "C:\Users\%username%\AppData\Local\CrashReportClient\Saved\Config\WindowsEditor\Engine.ini" /f
+del "C:\Users\%username%\AppData\Local\CrashReportClient\Saved\Logs\CrashReportClient.log" /f
+rmdir /q /s "C:\Users\%username%\AppData\Local\FortniteGame\"
+del "C:\Windows\Prefetch\BESERVICE.EXE-622E150D.pf" /f
+del "C:\Windows\Prefetch\CRASHREPORTCLIENT.EXE-C297728D.pf" /f
+del "C:\Windows\Prefetch\EASYANTICHEAT_SETUP.EXE-CF3441CE.pf" /f
+del "C:\Windows\Prefetch\FORTNITECLIENT-WIN64-SHIPPING-42C11B98.pf" /f
+del "C:\Windows\Prefetch\FORTNITECLIENT-WIN64-SHIPPING-5EAA410A.pf" /f
+del "C:\Windows\Prefetch\FORTNITELAUNCHER.EXE-AF00A2B5.pf" /f
+del "C:\Windows\Prefetch\RUNDLL32.EXE-F264FACF.pf" /f
+rmdir /q /s "C:\Users\%username%\AppData\Local\EpicGamesLauncher\Saved\"
+rmdir /q /s "C:\Program Files (x86)\Common Files\BattlEye"
+rmdir /q /s "C:\Users\%username%\AppData\Local\AMD\CN\GameReport"
+del "C:\Users\%username%\AppData\Local\AMD\CN\GameReport\FortniteClient-Win64-Shipping.exe" /f
+rmdir /q /s "C:\Users\%username%\AppData\Local\AMD\cl.cache"
+rmdir /q /s "C:\Users\%username%\AppData\Local\D3DSCache\e4548a4577c56a84"
+rmdir /q /s "C:\Users\%username%\AppData\Local\NVIDIA Corporation\GfeSDK"
+rmdir /q /s "C:\Users\%username%\AppData\Local\UnrealEngine\5.0"
+rmdir /q /s "C:\Users\%username%\AppData\Local\CrashReportClient"
+rmdir /q /s "C:\Users\%username%\AppData\Local\FortniteGame"
+reg delete "HKEY_CURRENT_USER\Software\Epic Games\Unreal Engine\Hardware Survey" /f 1>nul 2>nul
+reg delete "HKEY_CURRENT_USER\Software\Epic Games\Unreal Engine\Identifiers" /f 1>nul 2>nul
+reg delete "HKU\S-1-5-21-860440266-1445122309-108474356-1001\Software\Epic Games\Unreal Engine\Identifiers" /va /f 1>nul 2>nul
+reg delete "HKU\S-1-5-21-860440266-1445122309-108474356-1001\Software\Epic Games\Unreal Engine\Hardware Survey" /va /f 1>nul 2>nul
+reg delete "HKEY_CURRENT_USER\Software\Epic Games" /f 1>nul 2>nul
+reg delete "HKU\S-1-5-21-860440266-1445122309-108474356-1001\Software\Epic Games" /f 1>nul 2>nul
+REG ADD HKLM\SYSTEM\CurrentControlSet\Control\ComputerName\ComputerName /v ComputerName /t REG_SZ /d %random% /f 1>nul 2>nul
+REG ADD HKLM\SYSTEM\CurrentControlSet\Control\ComputerName\ActiveComputerName /v ComputerName /t REG_SZ /d %random% /f 1>nul 2>nul
+reg delete "HKEY_CURRENT_USER\Software\Epic Games" /f 1>nul 2>nul
+cls
+echo closed fortnite due to BE being launched
+echo you will need to gen a new account
+
+pause
+exit
+
+
+
+
+
+
+
+
+
+
